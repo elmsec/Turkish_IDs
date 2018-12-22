e00e6fbcdc2bae88a845228b70d4b7475b256a03
canelma <e.canelma@gmail.com>
2018-12-23 02:15:23 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Turkish_IDs
-Here to be used with peace of mind in order to validate the given number to be sure that if it's a valid Turkish ID (TC Kimlik No) or not, or to generate new and valid Turkish IDs as much as wanted.
+# What is this for?
+Here to be used with peace of mind in order to validate the given number to be sure that if it's a valid Turkish ID number (TC Kimlik No) or not, or to generate new and valid Turkish ID numbers as much as wanted.
+
+\**TC Kimlik No = Turkish ID (Number) = TC ID number*
+
+## How to use this?
+**Note:** `12345678901` is a placeholder for your possible TC ID number(s).
+
+### To validate the given TC ID number:
+```
+python turkish_ids.py -v 12345678901
+```
+or
+```
+python turkish_ids.py --validate 12345678901
+```
+
+### To generate new TC ID numbers:
+Let's say you want to generate 10 new TC ID numbers by starting from the TC ID number **12345678901**. So you can use the commands below:
+```
+python turkish_ids.py -g 10 -s 12345678901
+```
+or
+```
+python turkish_ids.py --generate 10 --start-from 12345678901
+```
---
+++ turkish_ids.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+import sys
+import argparse
+
+# ###############################
+# Eyup Can Elma - https://elma.pw
+# Dec 22, 2018 - 11:52:05 PM
+# ###############################
+
+
+def is_valid_tc_id(tc_id):
+    """
+    Checks that if the given number is a valid TC ID number or not.
+    """
+    tc_id = str(tc_id)
+
+    # We need to be sure that
+    # its first digit's greater than zero and
+    # total of its digits is as much as eleven digits.
+    if int(tc_id[0]) > 0 and len(tc_id) == 11:
+        # Now we need to check the eleventh digit
+        # Formula: n11 = (n1+n2+..+n10) mod 10
+        eleventh_digit = sum(map(int, tc_id[0:10])) % 10
+        if eleventh_digit == int(tc_id[10]):
+            # Calculate the tenth digit...
+            # Formula: n10 = ((n1+n3+n5+n7+n9)*7-(n2+n4+n6+n8)) mod 10
+            tenth_digit = (sum(map(int, tc_id[:10:2])) * 7-(
+                           sum(map(int, tc_id[1:9:2])))) % 10
+            # ..and verify that if it's true or not
+            if tenth_digit == int(tc_id[9]):
+                return True
+
+    return False
+
+
+def generate_valid_tc_ids(limit, start=11111111111, print_immediately=False):
+    """
+    By starting from the given number {start}, it generates new and valid
+    TC IDs as much as you specified with the parameter {limit}.
+    """
+    tc_ids = []
+    number = start
+    while len(tc_ids) < limit:
+        if is_valid_tc_id(number):
+            tc_ids.append(number)
+
+            if print_immediately:
+                print('=>', number)
+
+        number += 1
+    return tc_ids
+
+
+def check_args(args=None):
+    parser = argparse.ArgumentParser(
+        description='Turkish ID Validator and Generator App',
+        prefix_chars='-',
+    )
+    parser.add_argument(
+        '-v', '--validate',
+        metavar='12345678901',
+        help="Validate a number to be sure if it's a valid TC ID num. or not",
+        type=int,
+    )
+    parser.add_argument(
+        '-g', '--generate',
+        metavar='[1, ~]',
+        help=(
+            "Generate valid TC IDs up to the limit given with this. "
+            "You have to specify a sample TC ID number with the --start-from "
+            "or -s parameters to start generating new ones from."
+        ),
+        type=int
+    )
+    parser.add_argument(
+        '-s', '--start-from',
+        metavar='12345678901',
+        help=(
+            "Sample TC ID number to start generating new ones from. "
+            "This has to be used with the -g parameter."
+        ),
+        type=int,
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    all_args = check_args(sys.argv[1:])
+    if all_args.validate:
+        TC_ID = all_args.validate
+        v = 'a' if is_valid_tc_id(TC_ID) else 'NOT'
+
+        print('{tc_id} is {v} valid TC ID number.'.format(tc_id=TC_ID, v=v))
+        sys.exit(0)
+    elif all_args.generate:
+        if not all_args.start_from:
+            print(
+                'Please also specify a sample TC ID number as a start point '
+                'with the -s or --start-from parameter '
+                'so we can generate new ones from.'
+                )
+            sys.exit(2)
+
+        generate_valid_tc_ids(all_args.generate, all_args.start_from, True)
+    else:
+        print('Huh. What should I do?')
+        sys.exit(2)
